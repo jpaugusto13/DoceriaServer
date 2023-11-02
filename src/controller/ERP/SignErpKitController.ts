2601ce15ddc6cc7c869f333f1021dd9d7b85915b
jpaugusto13 <jpaugusto13@proton.me>
2023-11-02 18:27:24 -0300

    .

--- src/controller/ERP/SignErpKitController.ts
+++ src/controller/ERP/SignErpKitController.ts
@@ -12,7 +12,7 @@ async function SignErpKitController(req: Request, res: Response) {
   await database.query(insertKitQuery).then(() => {
     return res.status(201).json({ message: "Kit criado com sucesso!" });
   }).catch((e) => {
-    return res.status(400).json({ message: "Erro ao adicionar no banco\n"+e });
+    return res.status(400).json({ message: "Erro ao adicionar no banco: "+e });
   });
 }
 
