08748e9fc72ce1dd58c5029985875dcb95311c0c
jpaugusto13 <jpaugusto13@proton.me>
2024-02-08 15:56:14 -0300

    .

--- src/types/SaleType.ts
+++ src/types/SaleType.ts
@@ -4,7 +4,6 @@ import ProdutType from "./ProductType";
 type SaleType = {
   id: number;
   pagamento: PaymentType[];
-  valor: number;
   data_venda: Date;
   hora_venda: string;
   produtos: ProdutType[];
